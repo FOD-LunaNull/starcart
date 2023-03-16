d832d65b83ef56603f3909f00f77b8d865348e88
g-du-1 <dudaszeg1@gmail.com>
2023-03-16 18:39:33 +0000

    Wire up the characters panel to the API.

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,4 +1,39 @@
+import { Card, Loader, Message } from 'semantic-ui-react';
+import { useGetCharactersQuery } from '../services/swapApi';
+import { nanoid } from '@reduxjs/toolkit';
+
 const Characters = () => {
-	return <h4>add characters component</h4>
-}
-export default Characters
+	const { data, isError, isLoading } = useGetCharactersQuery();
+
+	if (isLoading) {
+		return <Loader active={isLoading} />;
+	}
+
+	if (isError) {
+		return <Message error={isError}>There was an error</Message>;
+	}
+
+	if (data && Boolean(data?.results?.length)) {
+		return (
+			<Card.Group centered>
+				{data.results.map(character => (
+					<Card key={nanoid()}>
+						<Card.Content>
+							<Card.Header>{character.name}</Card.Header>
+
+							{character.films && (
+								<Card.Meta>Films: {character.films.length}</Card.Meta>
+							)}
+						</Card.Content>
+					</Card>
+				))}
+			</Card.Group>
+		);
+	} else if (data?.results?.length === 0) {
+		return <Message warning>No characters found.</Message>;
+	}
+
+	return null;
+};
+
+export default Characters;
